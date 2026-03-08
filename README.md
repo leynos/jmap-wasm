c02deff025e2922c35b189856b832510b559cbd5
Payton McIntosh <pmcintosh@df12.net>
2026-03-08 22:34:27 +0000

    Add repo skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# IMAP WASM Plugin for IronClaw
+
+This is a generated project using [Copier](https://copier.readthedocs.io/).
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! `IMAP WASM Plugin for IronClaw` library.
+
+// TODO: Remove this stub and implement actual library functionality.
+/// Returns a greeting for the library.
+#[must_use]
+pub const fn greet() -> &'static str {
+    "Hello from IMAP WASM Plugin for IronClaw!"
+}
